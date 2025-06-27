cc2046aa2e61a9aa04a392aa67f497ec134e5805
chagasbarros <chagasbarros@outlook.com>
2025-06-27 19:49:39 -0300

    criando componente CriarEnquete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import Login from './components/login'
+import CriarEnquete from './components/CriarEnquete'
 
 function App() {
 
 
   return (
     <>
-      <Login/>
+      <CriarEnquete/>
     </>
   )
 }
